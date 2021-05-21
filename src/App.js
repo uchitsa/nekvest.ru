f06d38d4de116a6aa190d1554188b192ecbc1a94
DmitryBerdnikov <>
2021-05-21 22:17:49 +0300

    added bootstrap-icons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-// import logo from './logo.svg';
-import './App.scss';
+import { Container, Row, Col } from 'react-bootstrap';
+import { HeartFill } from 'react-bootstrap-icons';
+import './scss/main.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className=" App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <header className="test">
+          <nav>
+            <HeartFill />
+          </nav>
+        </header>
+      </Container>
     </div>
   );
 }
