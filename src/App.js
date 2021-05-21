0568efc71d248c6a69271792871a39ffeb8d275f
DmitryBerdnikov <>
2021-05-21 23:18:43 +0300

    added header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
-import { Container, Row, Col } from 'react-bootstrap';
-import { HeartFill } from 'react-bootstrap-icons';
+import { Nav } from 'react-bootstrap';
+import { Heart, Star, QuestionCircle, Person } from 'react-bootstrap-icons';
 import './scss/main.scss';
 
 function App() {
   return (
     <div className="App">
-      <Container>
-        <header className="test">
-          <nav>
-            <HeartFill />
-          </nav>
-        </header>
-      </Container>
+      <header className="App-header">
+        <div className="App-header__container">
+          <a className="App-header__logo" href="/">
+            Nekvest.ru
+          </a>
+          <Nav className="App-header__nav" activeKey="/popular">
+            <Nav.Item className="App-header__nav-item">
+              <Nav.Link className="App-header__nav-link" href="/top">
+                <Star className="App-header__icon" />
+                <span className="App-header__link-text">Топ-рейтинг</span>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item className="App-header__nav-item">
+              <Nav.Link className="App-header__nav-link" href="/popular">
+                <Heart className="App-header__icon" />
+                <span className="App-header__link-text">Поулярное</span>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item className="App-header__nav-item">
+              <Nav.Link className="App-header__nav-link" href="/questions">
+                <QuestionCircle className="App-header__icon" />
+                <span className="App-header__link-text">Мои вопросы</span>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item className="App-header__nav-item">
+              <Nav.Link className="App-header__nav-link" href="/profile">
+                <Person className="App-header__icon" />
+                <span className="App-header__link-text">Профиль</span>
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </div>
+      </header>
     </div>
   );
 }
