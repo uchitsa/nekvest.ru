654546fca91f0ab607f6fd751445ead122dcd989
DmitryBerdnikov <>
2021-05-22 08:27:23 +0300

    started app-top

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import { Nav } from 'react-bootstrap';
-import { Heart, Star, QuestionCircle, Person } from 'react-bootstrap-icons';
+import {
+  Heart,
+  Star,
+  QuestionCircle,
+  Person,
+  PencilSquare,
+} from 'react-bootstrap-icons';
 import avatar from './images/avatar.jpg';
 import './scss/main.scss';
 
@@ -21,7 +27,7 @@ function App() {
             <Nav.Item className="App-header__nav-item">
               <Nav.Link className="App-header__nav-link" href="/popular">
                 <Heart className="App-header__icon" />
-                <span className="App-header__link-text">Поулярное</span>
+                <span className="App-header__link-text">Лучшие</span>
               </Nav.Link>
             </Nav.Item>
             <Nav.Item className="App-header__nav-item">
@@ -39,9 +45,18 @@ function App() {
           </Nav>
         </div>
       </header>
+      <div className="App-top">
+        <div className="App-top__container container-md">
+          <h2 className="App-top__title">Профиль</h2>
+          <div class="App-top__end">
+            <a>
+              <PencilSquare className="App-top__icon" />
+            </a>
+          </div>
+        </div>
+      </div>
       <div className="Profile pt-3">
         <div className="container-md">
-          <h2 className="h2 mb-3">Профиль</h2>
           <div className="Profile__content">
             <img className="Profile__avatar Avatar" src={avatar} alt="Аватар" />
             <div className="Profile__name">Дмитрий Бердников</div>
